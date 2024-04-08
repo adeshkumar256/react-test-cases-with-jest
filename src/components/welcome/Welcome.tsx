4bc0d689686c3423226e6529dc102edd4a5fbe08
adeshkumar1201 <adesh@globaltestingexperts.com>
2024-04-08 20:01:16 +0530

    added one more component to demonstrate grouping of test cases using describe

---
+++ src/components/welcome/Welcome.tsx
@@ -0,0 +1,13 @@
+type AppName = {
+  appName?: string;
+};
+
+function Welcome(props: AppName) {
+  return (
+    <div>
+      <h2>Welcome to the {props.appName}</h2>
+    </div>
+  );
+}
+
+export default Welcome;
