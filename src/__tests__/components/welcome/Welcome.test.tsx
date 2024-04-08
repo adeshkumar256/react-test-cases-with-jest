25fb9a87a429188c98062438e0c95d9a756bb41f
adeshkumar1201 <adesh@globaltestingexperts.com>
2024-04-08 20:23:44 +0530

    moved tests to separate folder

--- src/__tests__/components/welcome/Welcome.test.tsx
+++ src/__tests__/components/welcome/Welcome.test.tsx
@@ -3,27 +3,29 @@ import Welcome from "../../../components/welcome/Welcome";
 
 //describe is used to group test cases
 describe("Welcome", () => {
-  test("component renders successfully", () => {
+  it("component renders successfully", () => {
     render(<Welcome />);
     const welcomeText = screen.getByText(/Welcome to the/i);
     expect(welcomeText).toBeInTheDocument();
   });
 
-  test("App name Facebook", () => {
+  it("App name Facebook", () => {
     render(<Welcome appName="facebook" />);
     const welcomeText = screen.getByText(/Welcome to the Facebook/i);
     expect(welcomeText).toBeInTheDocument();
   });
-  // use test.only to run particular test
-  //use.test.skip to skip tests
-  test("component renders successfully", () => {
+  
+  // use fit in place of test.only to run particular test with `it`
+  //use xit in place of test.skip to skip tests with `it`
+
+  it("component renders successfully", () => {
     render(<Welcome appName="instagram" />);
     const welcomeText = screen.getByText(/Welcome to the instagram/i);
     expect(welcomeText).toBeInTheDocument();
   });
 
   describe("testing twitter nested", () => {
-    test("component renders successfully", () => {
+    it("component renders successfully", () => {
       render(<Welcome appName="twitter" />);
       const welcomeText = screen.getByText(/Welcome to the twitter/i);
       expect(welcomeText).toBeInTheDocument();
@@ -32,7 +34,7 @@ describe("Welcome", () => {
 });
 
 describe("testing tiktok nested", () => {
-  test("component renders successfully", () => {
+  it("component renders successfully", () => {
     render(<Welcome appName="tiktok" />);
     const welcomeText = screen.getByText(/Welcome to the tiktok/i);
     expect(welcomeText).toBeInTheDocument();
